7c82c8cfbb7a0b7550f0da2e33c2335798726679
Guilherme Ramos <gramos1995@gmail.com>
2016-10-25 10:49:02 +0100

    Added parsing using PhpParse framework

--- index.php
+++ index.php
@@ -2,4 +2,24 @@
 
 require_once 'vendor/PhpParser/Autoloader.php';
 
-echo 'Yes';
+PhpParser\Autoloader::register();
+
+use PhpParser\Error;
+use PhpParser\ParserFactory;
+
+
+$parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP5);
+
+$code = '<?php $nis=$_POST[\'nis\'];
+$query="SELECT *FROM siswa WHERE nis=\'$nis\'";
+$q=mysql_query($query,$koneksi); ?>';
+
+try {
+    $stmts = $parser->parse($code);
+    echo 'All worked fine';
+    echo '<pre>';
+    print_r($stmts);
+    echo '</pre>';
+} catch (Error $e) {
+    echo 'Parse Error: ', $e->getMessage();
+}
