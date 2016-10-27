1b15f5446ede1c05c52eb266f826bc41033b18b1
Guilherme Ramos <gramos1995@gmail.com>
2016-10-27 14:45:52 +0100

    Added upload of file to a PHP string

--- classes/PHPSecurityInspector.php
+++ classes/PHPSecurityInspector.php
@@ -1,10 +1,28 @@
 <?php
 
+require_once 'vendor/PhpParser/Autoloader.php';
+
+PhpParser\Autoloader::register();
+
+use PhpParser\Error;
+use PhpParser\ParserFactory;
+
 class PHPSecurityInspector {
+    private $_parser;
+
     /**
      * @param String    $code       Code to be inspected
      */
     public function __construct($code) {
+        $this->_parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP5);
 
+        try {
+            $stmts = $this->_parser->parse($code);
+            echo '<pre>';
+            print_r($stmts);
+            echo '</pre>';
+        } catch (Error $e) {
+            echo 'Parse Error: ', $e->getMessage();
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <?php
 
-require_once 'vendor/PhpParser/Autoloader.php';
+require_once 'classes/PHPSecurityInspector.php';
 
-PhpParser\Autoloader::register();
-
-use PhpParser\Error;
-use PhpParser\ParserFactory;
-
-
-$parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP5);
-
-$code = '<?php $nis=$_POST[\'nis\'];
-$query="SELECT *FROM siswa WHERE nis=\'$nis\'";
-$q=mysql_query($query,$koneksi); ?>';
-
-try {
-    $stmts = $parser->parse($code);
-    echo 'All worked fine';
+if (isset($_POST['submit']) && isset($_FILES['code-to-check'])) {
+    $code = file_get_contents($_FILES['code-to-check']['tmp_name']);
     echo '<pre>';
-    print_r($stmts);
+    var_dump($code);
     echo '</pre>';
-} catch (Error $e) {
-    echo 'Parse Error: ', $e->getMessage();
-}
+
+    /*
+    $code = '<?php $nis=$_POST[\'nis\'];
+    $query="SELECT *FROM siswa WHERE nis=\'$nis\'";
+    $q=mysql_query($query,$koneksi); ?>';
+
+    $inspector = new PHPSecurityInspector($code);*/
+} else {
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHPSecurityInspector Demo</title>
+    </head>
+    <body>
+        <form method="POST" enctype="multipart/form-data">
+            <input type="file" name="code-to-check" />
+            <input type="submit" value="Submeter" name="submit" />
+        </form>
+    </body>
+</html>
+<?php } ?>
