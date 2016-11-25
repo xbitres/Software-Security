35f05a03fecc199e9274e85084bf1bb89c22344a
Guilherme Ramos <gramos1995@gmail.com>
2016-11-25 14:56:44 +0000

    Added support for command line usage

--- run.php
+++ run.php
@@ -2,16 +2,35 @@
 
 require_once 'classes/PHPSecurityInspector.php';
 
-print_r($argv);
-
-if (isset($_POST['submit']) && isset($_FILES['code-to-check'])) {
-    $file = file_get_contents($_FILES['code-to-check']['tmp_name']);
-    if (strpos($file, '<?php') !== false) {
-        $code = $file;
-    } else {
-        $code = '<?php ' . $file;
-    }
-
-
-    $inspector = new PHPSecurityInspector($code);
+if (!isset($argv[1])) {
+    echo "No file selected.\n";
+    exit();
 }
+
+echo "File to analysed: \t" . $argv[1] . "\n";
+
+$filename = $argv[1];
+
+if (!file_exists($filename)) {
+    echo "The file you inserted does not exist.\n";
+    exit();
+}
+
+$fid = fopen($filename, "r");
+
+if (!$fid) {
+    echo "You do not have permission to access the file.\n";
+    exit();
+}
+
+$file = fread($fid, filesize($filename));
+
+echo "File contents:\n$file\n";
+
+if (strpos($file, '<?php') !== false) {
+    $code = $file;
+} else {
+    $code = '<?php ' . $file;
+}
+
+$inspector = new PHPSecurityInspector($code);
